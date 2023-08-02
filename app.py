e3e972048340ac6560eb0c70d0ea1f83b3ffe9a3
Robert <mail@robertdarby.com.au>
2023-08-02 20:14:41 +1000

    fix: :bug: fixed incorrect symbol

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ import asyncio
 async def main() -> None:
     await postgres.insert_json_data("fundamentals", eod.get_live_delayed("AAPL.US"))
     await postgres.insert_json_data("live_delayed", eod.get_live_delayed("AAPL.US"))
-    await eod.get_live_socket("AMZN.TSLA")
+    await eod.get_live_socket("AMZN,TSLA")
 
 asyncio.run(main())
